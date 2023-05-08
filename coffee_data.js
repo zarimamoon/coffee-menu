41df359b4dc9bc6036d390bab90cd9c1cac8804d
Zarima Moon <zarima1991@yahoo.com>
2023-05-08 01:39:57 -0700

    changes to index.js file

--- coffee_data.js
+++ coffee_data.js
@@ -7,47 +7,56 @@
 // c. Objects and properties should have commas seperating them.
 // d. Connect the coffee_data.js file to the index.js file.
 
+// Prompt 1:
+// Clean the coffee_data file:
+
+// Checklist:
+// a. All prices should be numbers.
+// b. All items should be strings.
+// c. Objects and properties should have commas seperating them.
+// d. Connect the coffee_data.js file to the index.js file.
+
 const coffeeMenu = [
     {
         name: "cappuccino",
-        price: "8",
+        price: 8,
         seasonal: false,
     },
     {
-        name: espresso,
+        name: "espresso",
         price: 5,
         seasonal: false,
-    }
+    },
     {
         name: "latte",
         price: 5,
         seasonal: false,
     },
     {
-        name: affogato,
-        price: "9",
+        name: "affogato",
+        price: 9,
         seasonal: true,
     }, 
     {
         name: "macchiato",
-        price: "6",
-        seasonal: false,
-    }
-    {
-        name: americano,
-        price: 7,
-        seasonal: false,
-    }
-    {
-        name: "iced coffee",
-        price: "6",
+        price: 6,
         seasonal: false,
     },
     {
-        name: frappe,
-        price: "8",
+        name: "americano",
+        price: 7,
         seasonal: false,
-    }
+    },
+    {
+        name: "iced coffee",
+        price: 6,
+        seasonal: false,
+    },
+    {
+        name: "frappe",
+        price: 8,
+        seasonal: false,
+    },
     {
         name: "cuban espresso",
         price: 10,
@@ -56,3 +65,4 @@ const coffeeMenu = [
 ]
 
 module.exports = coffeeMenu
+
--- index.js
+++ index.js
@@ -5,54 +5,60 @@
 // a. All prices should be numbers.
 // b. All items should be strings.
 // c. Objects and properties should have commas seperating them.
+
+const coffeeMenu = require("./coffee_data.js");
 // d. Connect the coffee_data.js file to the index.js file.
 
-const coffeeMenu = [
-    {
-        name: "cappuccino",
-        price: "8",
-        seasonal: false,
-    },
-    {
-        name: espresso,
-        price: 5,
-        seasonal: false,
-    }
-    {
-        name: "latte",
-        price: 5,
-        seasonal: false,
-    },
-    {
-        name: affogato,
-        price: "9",
-        seasonal: true,
-    }, 
-    {
-        name: "macchiato",
-        price: "6",
-        seasonal: false,
-    }
-    {
-        name: americano,
-        price: 7,
-        seasonal: false,
-    }
-    {
-        name: "iced coffee",
-        price: "6",
-        seasonal: false,
-    },
-    {
-        name: frappe,
-        price: "8",
-        seasonal: false,
-    }
-    {
-        name: "cuban espresso",
-        price: 10,
-        seasonal: true,
-    }
-]
+// Promt 2:
+// Print an array of all the drinks on the menu.
+const printDrinks = (item) => {
+    return item.name;
+}
+console.log(coffeeMenu.map(printDrinks));
 
-module.exports = coffeeMenu
+// Promt 3:
+// Print an array of drinks that cost 5 and under.
+const underFive = (item) => {
+    return item.price <= 5;
+}
+const itemsUnderFive = coffeeMenu.filter(underFive);
+console.table(itemsUnderFive);
+
+// Promt 4
+// Print an array of drinks that are priced at an even number.
+const evenPrice = (item) => {
+    return item.price % 2 === 0;
+}
+const evenPricedItems = coffeeMenu.filter(evenPrice);
+console.table(evenPricedItems);
+
+// Promt 5:
+// Print the total if you were to order one of every drink.
+const sumTotalCost = (accumulator, currentValue) => {
+    return accumulator + currentValue.price;
+};
+
+const totalPrice = coffeeMenu.reduce(sumTotalCost, 0);
+console.log(totalPrice);
+
+// Promt 6:
+// Print an array with all the drinks that are seasonal.
+const isSeasonal = (item) => {
+    return item.seasonal === true;
+}
+const seasonalItems = coffeeMenu.filter(isSeasonal);
+console.table(seasonalItems);
+
+// Promt 7:
+// Print all the seasonal drinks with the words "with imported beans" after the item name. For example: "affogato with imported beans".
+const imported = (item) => {
+    return item.seasonal === true;
+};
+
+const importedBeans = coffeeMenu.filter(imported);
+
+for (const item of importedBeans) {
+    item.name += " with imported beans";
+}
+
+console.table(importedBeans);
